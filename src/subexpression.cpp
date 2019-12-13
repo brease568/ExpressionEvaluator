df0cd4f0e739302007ff24b2c6f7015f84671cab
Brian <b.rease568@gmail.com>
2019-12-12 19:41:53 -0800

    Added feature extension to allow evaluating conditional and negation expressions

--- src/subexpression.cpp
+++ src/subexpression.cpp
@@ -13,6 +13,8 @@ using namespace std;
 #include "equals.h"
 #include "and.h"
 #include "or.h"
+#include "conditional.h"
+#include "negation.h"
 
 SubExpression::SubExpression(Expression* left, Expression* right)
 {
@@ -24,33 +26,55 @@ Expression* SubExpression::parse()
 {
     Expression* left;
     Expression* right;
-    char operation, paren;
+    Expression* condition; //Added for conditional operator
+    char operation, paren, question;
     
     left = Operand::parse();
     cin >> operation;
-    right = Operand::parse();
-    cin >> paren;
-    switch (operation)
+
+    //First 'if' evaluates if the expression is a conditional expression
+    if(operation == ':')
     {
-        case '+':
-            return new Plus(left, right);
-        case '-':
-            return new Minus(left, right);
-        case '*':
-            return new Times(left, right);
-        case '/':
-            return new Divide(left, right);
-	//Adding support for relational and logical operators:
-	case '>':
-	    return new Greater(left, right);
-	case '<':
-	    return new Less(left, right);
-	case '=':
-	    return new Equals(left, right);
-	case '&':
-	    return new And(left, right);
-	case '|':
-	    return new Or(left, right);
+	//The third operand represents the condition in the grammar
+	right = Operand::parse();
+	cin >> question;
+	condition = Operand::parse();
+	cin >> paren;
+	return new Conditional(left, right, condition);
+    }
+    //Second 'if' evaluates if the expression is a negation expression
+    else if(operation == '!')
+    {
+	cin >> paren;
+	return new Negation(left, NULL); //This expression should only contain one operand
+    }
+    //Else the expression is one of the following switch case expressions
+    else
+    {
+    	right = Operand::parse();
+    	cin >> paren;
+    	switch (operation)
+    	{
+            case '+':
+            	return new Plus(left, right);
+            case '-':
+            	return new Minus(left, right);
+            case '*':
+    	        return new Times(left, right);
+            case '/':
+            	return new Divide(left, right);
+	    //Adding support for relational and logical operators:
+	    case '>':
+	    	return new Greater(left, right);
+	    case '<':
+	    	return new Less(left, right);
+	    case '=':
+	    	return new Equals(left, right);
+	    case '&':
+	    	return new And(left, right);
+	    case '|':
+	    	return new Or(left, right);
+    	}
     }
     return 0;
 }
